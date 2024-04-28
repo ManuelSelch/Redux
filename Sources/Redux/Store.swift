84f30462f1b4358965650a9ffad3816f92f0a926
Manuel Selch <manuel@selch.de>
2024-04-28 18:19:58 +0200

    redux dependencies

--- Sources/Redux/Store.swift
+++ Sources/Redux/Store.swift
@@ -1,37 +1,41 @@
 import Foundation
 import Combine
 
-@available(iOS 16.0, *)
-public typealias Reducer<State, Action> = (inout State, Action) -> AnyPublisher<Action, Error>?
 
 @available(iOS 16.0, *)
-public typealias Middleware<State, Action> = (State, Action) -> AnyPublisher<Action, Never>?
+public typealias Reducer<State, Action, Dependencies> = (inout State, Action, Dependencies) -> AnyPublisher<Action, Error>?
+
+@available(iOS 16.0, *)
+public typealias Middleware<State, Action, Dependencies> = (State, Action, Dependencies) -> AnyPublisher<Action, Never>?
 
 
  
 @available(iOS 16.0, *)
-public class Store<State, Action>: ObservableObject {
+public class Store<State, Action, Dependencies>: ObservableObject {
     @Published public private(set) var state: State
     
     private var cancellables: Set<AnyCancellable> = []
     
-    private let reducer: Reducer<State, Action>
-    private var middlewares:  [Middleware<State, Action>]
+    public let dependencies: Dependencies
+    private let reducer: Reducer<State, Action, Dependencies>
+    private let middlewares:  [Middleware<State, Action, Dependencies>]
 
     public init(
         initialState: State, 
-        reducer: @escaping Reducer<State, Action>,
-        middlewares: [Middleware<State, Action>] = []
+        reducer: @escaping Reducer<State, Action, Dependencies>,
+        dependencies: Dependencies,
+        middlewares: [Middleware<State, Action, Dependencies>] = []
     ) {
         self.state = initialState
         self.reducer = reducer
+        self.dependencies = dependencies
         self.middlewares = middlewares
     }
 
     public func send(_ action: Action) {
         handleLog(action)
         
-        guard let effect = reducer(&state, action) else {
+        guard let effect = reducer(&state, action, dependencies) else {
             return
         }
  
@@ -51,7 +55,7 @@ public class Store<State, Action>: ObservableObject {
         
         middlewares.forEach { middleware in
             
-            guard let publisher = middleware(state, action) else {
+            guard let publisher = middleware(state, action, dependencies) else {
                 return
             }
             
@@ -63,17 +67,19 @@ public class Store<State, Action>: ObservableObject {
     }
     
     
-    public func lift<DerivedState: Equatable, ExtractedAction>(
+    public func lift<DerivedState: Equatable, ExtractedAction, DerivedDependencies>(
         _ deriveState: @escaping (State) -> DerivedState,
-        _ embedAction: @escaping (ExtractedAction) -> Action
-    ) -> Store<DerivedState, ExtractedAction> {
+        _ embedAction: @escaping (ExtractedAction) -> Action,
+        _ derivedDependencies: DerivedDependencies
+    ) -> Store<DerivedState, ExtractedAction, DerivedDependencies> {
         
-        let derivedStore = Store<DerivedState, ExtractedAction>(
+        let derivedStore = Store<DerivedState, ExtractedAction, DerivedDependencies>(
             initialState: deriveState(state),
-            reducer: { derivedState, action  in
+            reducer: { derivedState, action, dependencies  in
                 self.send(embedAction(action))
                 return Empty().eraseToAnyPublisher()
-            }
+            },
+            dependencies: derivedDependencies
         )
         
         $state
