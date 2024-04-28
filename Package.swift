4ee2c9ba55122c12c0e32d7bd195d959e1a979d8
Manuel Selch <manuel@selch.de>
2024-04-28 17:37:47 +0200

    router

--- Package.swift
+++ Package.swift
@@ -5,17 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "Redux",
+    platforms: [
+        .iOS(.v16), .iOS(.v17)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Redux",
             targets: ["Redux"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Redux"),
+        .target(name: "Redux"),
         .testTarget(
             name: "ReduxTests",
             dependencies: ["Redux"]),
