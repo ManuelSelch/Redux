d2e47c020a1d25970ec06d1135004729c5644dde
Manuel Selch <manuel@selch.de>
2024-05-14 23:02:31 +0200

    use async await request function

--- Sources/Redux/Service.swift
+++ Sources/Redux/Service.swift
@@ -17,24 +17,24 @@ public extension IService {
             .eraseToAnyPublisher()
     }
     
-    func request<Response: Decodable, TargetType>(_ provider: MoyaProvider<TargetType>, _ method: TargetType) -> AnyPublisher<Response, Error> {
-        return Future<Response, Error> { promise in
+    func request<Response: Decodable, TargetType>(_ provider: MoyaProvider<TargetType>, _ method: TargetType) async throws -> Response {
+        return try await withCheckedThrowingContinuation { continuation in
             provider.request(method){ result in
                 switch result {
                 case .success(let response):
                     if let data = try? JSONDecoder().decode(Response.self, from: response.data) {
-                        promise(.success(data))
+                        continuation.resume(returning: data)
                     }else {
                         if let string = String(data: response.data, encoding: .utf8) {
-                            promise(.failure(ReduxError.unknown(method.path + " -> " + string)))
+                            continuation.resume(throwing: ReduxError.unknown(method.path + " -> " + string))
                         }else{
-                            promise(.failure(ReduxError.decodeFailed))
+                            continuation.resume(throwing: ReduxError.decodeFailed)
                         }
                     }
                 case .failure(let error):
-                    promise(.failure(ReduxError.unknown(method.path + " -> " + error.localizedDescription)))
+                    continuation.resume(throwing: ReduxError.unknown(method.path + " -> " + error.localizedDescription))
                 }
             }
-        }.eraseToAnyPublisher()
+        }
     }
 }
